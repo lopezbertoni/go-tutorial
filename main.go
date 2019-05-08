24c450a3840f7c27f8abd078c7454bd2f14e7789
Federico Javier Lopez Bertoni <lopezbertoni@gmail.com>
2019-05-08 16:39:29 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+
+	var x int
+	x = 3
+	fmt.Println("x: ", x)
+
+	var y = 20
+	fmt.Println("y: ", y)
+
+	var i, j = 100, "hello"
+	fmt.Println("i and j:", i, j)
+}
